b34623543c2c55e4a73e1b41e50343a4b0bf5862
jcnpereira <casimiro@ipt.pt>
2026-03-11 10:21:50 +0000

    adição de sumários às classes

--- API/API/Models/Photography.cs
+++ API/API/Models/Photography.cs
@@ -1,17 +1,38 @@
 ﻿namespace API.Models {
 
+   /// <summary>
+   /// objetos a serem vendidos na loja
+   /// </summary>
    public class Photography {
 
+      /// <summary>
+      /// PK
+      /// </summary>
       public int Id { get; set; }
 
+      /// <summary>
+      /// nome associado à fotografia
+      /// </summary>
       public string Title { get; set; }
 
+      /// <summary>
+      /// descrição (opcional) da fotografia
+      /// </summary>
       public string Description { get; set; }
 
+      /// <summary>
+      /// nome do ficheiro que contém a fotografia
+      /// </summary>
       public string File { get; set; }
 
+      /// <summary>
+      /// Data em que a fotografia foi tirada
+      /// </summary>
       public DateTime Date { get; set; }
 
+      /// <summary>
+      /// Preço de venda da fotografia
+      /// </summary>
       public decimal Price { get; set; }
 
    }
--- API/API/Models/Purchase.cs
+++ API/API/Models/Purchase.cs
@@ -1,16 +1,32 @@
 ﻿using Mono.TextTemplating;
 
 namespace API.Models {
+
+   /// <summary>
+   /// dados da compra de fotografias, por um utilizador
+   /// </summary>
    public class Purchase {
 
+      /// <summary>
+      /// PK
+      /// </summary>
       public int Id { get; set; }
 
+      /// <summary>
+      /// Data da compra
+      /// </summary>
       public DateTime Date { get; set; }
 
+      /// <summary>
+      /// Estado (situação) em que se encontra a compra
+      /// </summary>
       public State State { get; set; }
 
    }
 
+   /// <summary>
+   /// Possíveis estados associados a uma compra
+   /// </summary>
    public enum State {
       Pending,
       Paid,
