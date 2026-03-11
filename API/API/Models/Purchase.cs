95364a415e1047e456ec9525603efb2fef0ca618
jcnpereira <casimiro@ipt.pt>
2026-03-11 10:06:26 +0000

    classes iniciais do Model

---
+++ API/API/Models/Purchase.cs
@@ -0,0 +1,26 @@
+﻿using Mono.TextTemplating;
+
+namespace API.Models {
+   public class Purchase {
+
+      public int Id { get; set; }
+
+      public DateTime Date { get; set; }
+
+      public State State { get; set; }
+
+   }
+
+   public enum State {
+      Pending,
+      Paid,
+      Sent,
+      Delivered,
+      Closed
+   }
+
+
+
+
+
+}
